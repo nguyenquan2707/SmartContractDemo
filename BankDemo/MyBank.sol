a410e59baa28d4e429848f86d3f3d3eaa02fa225
nhovay8 <nhovay8@gmail.com>
2019-10-13 15:03:59 +0700

    change implement for

--- BankDemo/MyBank.sol
+++ BankDemo/MyBank.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.1;
 
 interface GeneralInfo {
+    //abstract method
     function checkValue(uint256 _amount) external view returns(bool);
+    //abstract method
+    function loan() external view returns(bool);
 }
 
 contract Bank is GeneralInfo{
@@ -25,15 +28,16 @@ contract Bank is GeneralInfo{
         return amount;
     }
     
-    function loan() public view returns(bool);
+    // check withraw value compare with existing value
+    function checkValue(uint256 _amount) public view returns(bool) {
+        return amount >= _amount;
+    }
+    
+    // only for loan in case they have amout > 0
+    function loan() public view returns(bool) {
+        return amount > 0;
+    }
 }
 
 contract MyBank is Bank(2){
-    function loan() public view returns(bool) {
-        return true;
-    }
-    
-     function checkValue(uint256 _amount) public view returns(bool) {
-        return getBalance() >= _amount;
-    }
 }

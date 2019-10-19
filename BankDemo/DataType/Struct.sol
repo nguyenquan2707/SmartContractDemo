eb48c083e6b3a8014bfd4bdb33c63c6b3a255d10
nhovay8 <nhovay8@gmail.com>
2019-10-19 17:39:59 +0700

    Struct Type

--- BankDemo/DataType/Struct.sol
+++ BankDemo/DataType/Struct.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.11;
 
 contract Struct {
-    
+    uint8 public count;
     Person[] public listPerson; 
     struct Person {
         string lastName;
@@ -9,7 +9,13 @@ contract Struct {
         uint8 age;
     }
     
-    function addPerson(string memory _lastName, string memory _firtName, uint8 _age) public {
-        listPerson.push(Person(_lastName, _firtName, _age));
+    function addPerson(string memory _lastName, string memory _firstName, uint8 _age) public {
+        listPerson.push(Person(_lastName, _firstName, _age));
+        count+= 1;
+    }
+    
+    function removePerson(uint8 _index) public {
+       delete listPerson[_index];
+       count-=1;
     }
 }

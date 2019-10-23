0649d77f705f4b6e5800617108822431b8c42670
nhovay8 <nhovay8@gmail.com>
2019-10-23 23:19:07 +0700

    how to call parent constructor

--- Tokens/Inheritance.sol
+++ Tokens/Inheritance.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.11;
 contract ERC20Token {
     string public name;
     mapping(address => uint256) public balances;
+    constructor(string memory _name) public {
+        name = _name;
+    }
     
     function mint() public {
         balances[tx.origin] += 1; // address of whoever call this contract
@@ -11,5 +14,10 @@ contract ERC20Token {
 
 
 contract MyTokens is ERC20Token{
+    string public symbol;
+    //the way we call parent constructor, that easy
+    constructor (string memory _name, string memory _symbol) ERC20Token(_name) public {
+        symbol = _symbol;
+    }
     
 }

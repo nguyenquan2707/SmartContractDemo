95da2312b389b9a389c6fa1ed748062f5fac4a0e
nhovay8 <nhovay8@gmail.com>
2019-10-24 20:09:09 +0700

    Call Library in contract

--- Import/UseLibrary/UseLibraryContract.sol
+++ Import/UseLibrary/UseLibraryContract.sol
@@ -1,9 +1,14 @@
 pragma solidity ^0.5.11;
 
 library Match {
-    
+    function divide(uint256 a, uint256 b) public pure returns(uint256) {
+        require( b > 0 );
+        return a/b;
+    }
 }
 
 contract UseLibraryContract {
-    
+    function calculator(uint256 _value1, uint256 _value2) public returns (uint256) {
+       return Match.divide(_value1,_value2);
+    }
 }

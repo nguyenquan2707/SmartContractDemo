dbd69e45476390a1c7e49ced91692aa175faf725
nhovay8 <nhovay8@gmail.com>
2019-10-25 19:30:03 +0700

    add withdraw for escrow contract

--- EscrowContract/EscrowContract.sol
+++ EscrowContract/EscrowContract.sol
@@ -10,6 +10,13 @@ pragma solidity ^0.5.11;
      
      function deposist(address payee) public payable {
          require(msg.sender == agent);
-         deposits[payee] = msg.value;
+         deposits[payee] = deposits[payee] + msg.value;
+     }
+     
+     function withdraw(address payable payee) public {
+         require(msg.sender == agent);
+         uint256 amount = deposits[payee];
+         deposits[payee] = 0;
+         payee.transfer(amount);
      }
  }

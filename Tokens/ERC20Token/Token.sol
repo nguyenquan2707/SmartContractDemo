10931d7113b4ba89bdc411bce7078a3c9a80fabf
nhovay8 <nhovay8@gmail.com>
2019-10-26 12:22:44 +0700

    Add transferFrom function

--- Tokens/ERC20Token/Token.sol
+++ Tokens/ERC20Token/Token.sol
@@ -35,4 +35,12 @@ contract Token {
     emit Approval(msg.sender, _spender, _value);
     return true;
   }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+    allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+    _transfer(_from, _to, _value);
+    return true;
+  }
 }

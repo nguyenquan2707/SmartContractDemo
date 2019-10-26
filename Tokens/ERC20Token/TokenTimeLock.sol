6328fc8f3da44abc62890c5841b963f71caa0d68
nhovay8 <nhovay8@gmail.com>
2019-10-26 14:16:36 +0700

    fix releaseTime to _releaseTime

--- Tokens/ERC20Token/TokenTimeLock.sol
+++ Tokens/ERC20Token/TokenTimeLock.sol
@@ -6,15 +6,15 @@ contract TokenTimeLock {
     address public beneficiary;
     uint256 public releaseTime;
     
-    constructor(Token _token, address _beneficiary, uint256 _releaseTime) public{
-         require(releaseTime > block.timestamp);
-         token = _token; //contract address : 0x692a70d2e424a56d2c6c27aa97d1a86395877b3a
-         beneficiary = _beneficiary;
+    constructor(Token _token, address payable _beneficiary, uint256 _releaseTime) public{
+         require(_releaseTime > block.timestamp);
+         token = _token; //contract address : 0x08970FEd061E7747CD9a38d680A601510CB659FB
+         beneficiary = _beneficiary; // ex: 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
          releaseTime = _releaseTime;
      }
      
-     function release() public {
-         require(block.timestamp > releaseTime);
+     function release() public payable{
+         require(block.timestamp >= releaseTime);
          uint256 amount = token.balanceOf(address(this));//this refer to TokenTimeLock contract, 
          require(amount > 0);
          token.transfer(beneficiary, amount);

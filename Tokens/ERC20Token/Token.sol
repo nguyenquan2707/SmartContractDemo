5a9925c44b1e8d2a5f73b5b36cb75ad13640a236
nhovay8 <nhovay8@gmail.com>
2019-10-26 12:10:35 +0700

    Add Token.sol and variables

--- Tokens/ERC20Token/Token.sol
+++ Tokens/ERC20Token/Token.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.5.11;
 
-contract TokenTimeLock {
-    
+contract Token {
+  string public name = "DApp Token";
+  string public symbol = "DAPP";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 }

2526b21dc453ead0ed8efbe8804dc9aa950e58d1
nhovay8 <nhovay8@gmail.com>
2019-10-26 12:11:42 +0700

    Add event and totalSupply

--- Tokens/ERC20Token/Token.sol
+++ Tokens/ERC20Token/Token.sol
@@ -7,4 +7,12 @@ contract Token {
   uint256 public totalSupply;
   mapping(address => uint256) public balanceOf;
   mapping(address => mapping(address => uint256)) public allowance;
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+  constructor() public {
+    totalSupply = 1000000 * (10 ** decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
 }

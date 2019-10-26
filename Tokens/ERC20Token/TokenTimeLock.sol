8e76eddb8321debc353a686834611d5dc0c8f3f1
nhovay8 <nhovay8@gmail.com>
2019-10-26 13:41:01 +0700

    add variables for constructor

--- Tokens/ERC20Token/TokenTimeLock.sol
+++ Tokens/ERC20Token/TokenTimeLock.sol
@@ -4,9 +4,16 @@ import "./Token.sol";
 contract TokenTimeLock {
     Token public token;
     address public beneficiary;
-    uint256 public realeseTime;
+    uint256 public releaseTime;
     
-    constructor(Token _token) public{
-         token = _token;
+    constructor(Token _token, address _beneficiary, uint256 _releaseTime) public{
+         require(releaseTime > block.timestamp);
+         token = _token; //contract address : 0x692a70d2e424a56d2c6c27aa97d1a86395877b3a
+         beneficiary = _beneficiary;
+         releaseTime = _releaseTime;
+     }
+     
+     function release() public {
+         
      }
 }

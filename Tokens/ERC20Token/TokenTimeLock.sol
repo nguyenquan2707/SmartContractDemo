79a1e28d574bf5548c061a67072d10a30e4f8a7e
nhovay8 <nhovay8@gmail.com>
2019-10-26 13:18:51 +0700

    add constructor and variables

--- Tokens/ERC20Token/TokenTimeLock.sol
+++ Tokens/ERC20Token/TokenTimeLock.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.5.11;
 
 import "./Token.sol";
-//This contract will release at specific time
 contract TokenTimeLock {
+    Token public token;
+    address public beneficiary;
+    uint256 public realeseTime;
     
+    constructor(Token _token) public{
+         token = _token;
+     }
 }

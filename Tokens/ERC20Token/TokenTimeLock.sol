b3ae0b5c634b232be2c6c172278a96b966ed7292
nhovay8 <nhovay8@gmail.com>
2019-10-26 14:23:22 +0700

    remove payable

--- Tokens/ERC20Token/TokenTimeLock.sol
+++ Tokens/ERC20Token/TokenTimeLock.sol
@@ -6,14 +6,14 @@ contract TokenTimeLock {
     address public beneficiary;
     uint256 public releaseTime;
     
-    constructor(Token _token, address payable _beneficiary, uint256 _releaseTime) public{
+    constructor(Token _token, address _beneficiary, uint256 _releaseTime) public{
          require(_releaseTime > block.timestamp);
          token = _token; //contract address : 0x08970FEd061E7747CD9a38d680A601510CB659FB
          beneficiary = _beneficiary; // ex: 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
          releaseTime = _releaseTime;
      }
      
-     function release() public payable{
+     function release() public {
          require(block.timestamp >= releaseTime);
          uint256 amount = token.balanceOf(address(this));//this refer to TokenTimeLock contract, 
          require(amount > 0);

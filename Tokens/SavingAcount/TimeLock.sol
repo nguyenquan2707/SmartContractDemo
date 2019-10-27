7764353621bf88533ee03fae7744799d207d9ac9
nhovay8 <nhovay8@gmail.com>
2019-10-27 19:38:26 +0700

    add constructor

--- Tokens/SavingAcount/TimeLock.sol
+++ Tokens/SavingAcount/TimeLock.sol
@@ -5,4 +5,11 @@ contract TimeLock {
     // Who can withdraw
     //how much?
     // when
+    
+    address beneficiary;
+    uint256 releaseTime;
+    constructor(address _beneficiary, uint256 _releaseTime) public {
+        beneficiary = _beneficiary;
+        releaseTime = _releaseTime;
+    }
 }
